a9314dd2725b364f1072d8bda5ad82035c48c7f9
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-01-11 03:49:33 +0200

    Update main.cpp

--- Data structure/Linked List/main.cpp
+++ Data structure/Linked List/main.cpp
@@ -176,3 +176,197 @@ void Delete_end(){
         ptr->next = NULL;
     }
 }
+
+
+/////////////////////////////////////////////
+
+
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int item;
+    Node *next;
+};
+
+class LinkedList {
+private:
+    Node *head;
+    
+public:
+    // Constructor to initialize the list
+    LinkedList() {
+        head = NULL;
+    }
+
+    // Insert a node at the beginning of the list
+    void insert_first(Node d) {
+        Node *ptr = new Node;
+        *ptr = d;
+        ptr->next = head;
+        head = ptr;
+    }
+
+    // Insert a node at the end of the list
+    void insert_last(Node d) {
+        if (head == NULL) {
+            insert_first(d);
+            return;
+        }
+        Node *ptr = head;
+        while (ptr->next != NULL) {
+            ptr = ptr->next;
+        }
+        ptr->next = new Node;
+        ptr = ptr->next;
+        *ptr = d;
+        ptr->next = NULL;
+    }
+
+    // Insert an item in order
+    void insert_item_order(Node d) {
+        if (head == NULL || d.item < head->item) {
+            insert_first(d);
+            return;
+        }
+        Node *ptr = head;
+        while (ptr->next != NULL && ptr->next->item < d.item) {
+            ptr = ptr->next;
+        }
+        Node *temp = new Node;
+        *temp = d;
+        temp->next = ptr->next;
+        ptr->next = temp;
+    }
+
+    // Insert an item at a specific index
+    void insert_index(Node d, int n) {
+        if (n == 0 || head == NULL) {
+            insert_first(d);
+            return;
+        }
+        Node *ptr = head;
+        for (int k = 0; k < n && ptr->next != NULL; k++) {
+            ptr = ptr->next;
+        }
+        Node *temp = new Node;
+        *temp = d;
+        temp->next = ptr->next;
+        ptr->next = temp;
+    }
+
+    // Check if the list is empty
+    bool is_empty() {
+        return (head == NULL);
+    }
+
+    // Get the first element of the list
+    Node read_first() {
+        return *head;
+    }
+
+    // Count the number of nodes in the list
+    int number_of_nodes() {
+        int counter = 0;
+        Node *ptr = head;
+        while (ptr != NULL) {
+            counter++;
+            ptr = ptr->next;
+        }
+        return counter;
+    }
+
+    // Get the last element of the list
+    Node read_last() {
+        Node *ptr = head;
+        while (ptr->next != NULL) {
+            ptr = ptr->next;
+        }
+        return *ptr;
+    }
+
+    // Get data of a node by index
+    Node get_index_data(int index) {
+        Node *ptr = head;
+        for (int k = 0; k < index && ptr != NULL; k++) {
+            ptr = ptr->next;
+        }
+        return *ptr;
+    }
+
+    // Get data of a node by item
+    Node get_item_data(int item) {
+        Node *ptr = head;
+        while (ptr != NULL) {
+            if (ptr->item == item) {
+                return *ptr;
+            }
+            ptr = ptr->next;
+        }
+        Node temp;
+        temp.item = 0;
+        temp.next = NULL;
+        return temp;
+    }
+
+    // Delete the first element of the list
+    void delete_first() {
+        if (head != NULL) {
+            Node *ptr = head;
+            head = head->next;
+            delete ptr;
+        }
+    }
+
+    // Delete the last element of the list
+    void delete_last() {
+        if (head == NULL) return;
+        if (head->next == NULL) {
+            delete_first();
+            return;
+        }
+        Node *ptr = head;
+        while (ptr->next->next != NULL) {
+            ptr = ptr->next;
+        }
+        Node *temp = ptr->next;
+        ptr->next = NULL;
+        delete temp;
+    }
+
+    // Print all nodes in the list
+    void print_list() {
+        Node *temp = head;
+        while (temp != NULL) {
+            cout << temp->item << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    LinkedList list1;
+    Node temp;
+    cout << "Enter 5 numbers: ";
+    
+    // Input 5 numbers and insert them at the beginning of the list
+    for (int k = 0; k < 5; k++) {
+        cin >> temp.item;
+        temp.next = NULL;
+        list1.insert_first(temp);
+    }
+    
+    // Delete the last element and display the list
+    list1.delete_last();
+    
+    cout << "Data in Linked List: ";
+    list1.print_list();
+
+    // Search for an item
+    int x;
+    cin >> x;
+    temp = list1.get_item_data(x);
+    cout << "Found item: " << temp.item << endl;
+}
